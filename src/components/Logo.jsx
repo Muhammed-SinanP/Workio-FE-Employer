4d08aea3e2393028d9a1df63a9917b992fa1e301
Muhammed Sinan P <iam.muhammedsinan.p@gmail.com>
2025-03-28 22:09:47 +0530

    resize logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,14 +5,14 @@ import { useNavigate } from "react-router-dom";
 const Logo = () => {
   const navigate = useNavigate()
   return (
-    <div onClick={() => navigate("/")} className="cursor-pointer flex gap-0.5 items-center">
-      <img src={brandLogo} alt="brand logo" className="h-8" />
-      <div>
-        <h1 className="font-brand-font text-2xl text-brand">
+    <div onClick={() => navigate("/")} className="cursor-pointer scale-95 flex gap-0.5 items-center">
+      <img src={brandLogo} alt="brand logo" className="h-9" />
+      <div className='pb-0.5'>
+        <h1 className="font-brand-font text-2xl text-brand dark:text-brand-light">
           <span className="text-3xl">W</span>
           <span className="-ml-1">orkio</span>
         </h1>
-        <h2 className="-mt-2 text-xs font-brand-font text-brand-dark dark:text-white ml-4 ">
+        <h2 className="-mt-2 text-xs font-medium text-brand-dark dark:text-white ml-4 ">
           for employers
         </h2>
       </div>
